f37f4be1468d1c2b05d4a54cb40442a56222e236
parkchaewoon53 <parkchae53@gmail.com>
2025-05-02 17:40:10 +0900

    favion

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import logo from './assets/logo.png'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div className='container'>
+      <div className='nav'></div>
+      <img src={logo} alt="" />
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <a href="">갬성 포스트</a>
+        <a href="">갬성 로또</a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
